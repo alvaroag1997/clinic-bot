736d8798b17a420379ff867812125b2a5a84ff7d
alvaroag1997 <alvaroaguilargi@gmail.com>
2025-12-28 13:29:38 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import express from "express";
+
 const app = express();
 
+// Ruta de prueba
 app.get("/", (req, res) => {
-  res.send("ONLINE");
+  res.send("¡Servidor funcionando en Railway!");
 });
 
+// Puerto dinámico de Railway
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("ONLINE EN", PORT);
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
